3c0ef0759a4445b5e6af80740bd1e3e93f657957
Sarah Habershon <habershon@gmail.com>
2020-11-18 18:51:30 +0100

    script running

---
+++ script.py
@@ -0,0 +1,47 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+#read in population and codes
+pop = pd.read_csv("Europop.csv")
+ecjrc_load = pd.read_csv("https://raw.githubusercontent.com/ec-jrc/COVID-19/master/data-by-country/jrc-covid-19-all-days-by-country.csv", parse_dates = ["Date"])
+
+#set up ecjrc dataframe
+ecjrc_load = ecjrc_load.drop(["lat", "lon"], axis = 1)
+ecjrc_load = ecjrc_load.rename(columns={'NUTS': 'nuts'})
+
+#remove UK to avoid duplication
+ecjrc_load = ecjrc_load[ecjrc_load.nuts != "UK"]
+
+#download latest hospitalisation figures as csv: https://coronavirus.data.gov.uk/details/healthcare
+uk_subt_load = pd.read_csv("uk_by_nation.csv", parse_dates = ["date"])
+uk_nat_load = pd.read_csv("uk_total.csv", parse_dates = ["date"])
+
+#add NUTS codes for UK - note that there is no nuts code for England; I have added that the population table as "UKXYZ"
+uk_all = uk_subt_load.append(uk_nat_load)
+uk_all = uk_all.rename(columns={"areaName": "CountryName", "date": "Date", "hospitalCases": "Hospitalized"}, errors="raise")
+uk_all.loc[uk_all['CountryName'] == "Scotland", "nuts"] = "UKM"
+uk_all.loc[uk_all['CountryName'] == "Wales", "nuts"] = "UKL"
+uk_all.loc[uk_all['CountryName'] == "Northern Ireland", "nuts"] = "UKN"
+uk_all.loc[uk_all['CountryName'] == "United Kingdom", "nuts"] = "UK"
+uk_all.loc[uk_all['CountryName'] == "England", "nuts"] = "UKXYZ"
+uk_all.to_csv("test.csv")
+
+#add UK to the dataframe
+ecjrc_load = ecjrc_load.append(uk_all)
+
+#match with population table
+matched = pd.merge(ecjrc_load,
+                pop,
+                on = "nuts",
+                how = "inner")
+
+#get the rolling average
+matched["rolling_hosp"] = matched.groupby("nuts")['Hospitalized'].rolling(7).mean().reset_index(0,drop=True)
+matched["rolling_hosp_per_100000"] = 100000*(matched["rolling_hosp"]/matched["population"])
+
+#remove null values
+matched = matched.dropna(axis=0, subset=["rolling_hosp"])
+
+matched_hosp = matched[["Date", "CountryName", "Hospitalized", "rolling_hosp", "population", "rolling_hosp_per_100000"]]
+
+matched_hosp.to_csv("rolling_hospitalisations.csv")
